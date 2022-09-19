d83ec4ec0db3f0ed0dc27851db7dc46d3d49cf5f
Wlad Paiva <desenvolvedormg@gmail.com>
2022-09-19 15:08:09 -0300

    fix: build script

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import react from "@vitejs/plugin-react";
 // `unchained-bitcoin` package needs those polyfills to work on the browser
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
+import rollupNodePolyFill from "rollup-plugin-node-polyfills";
+import inject from "@rollup/plugin-inject";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,6 +18,20 @@ export default defineConfig({
         }),
         NodeModulesPolyfillPlugin(),
       ],
+    },
+  },
+  resolve: {
+    alias: {
+      stream: "rollup-plugin-node-polyfills/polyfills/stream",
+      events: "rollup-plugin-node-polyfills/polyfills/events",
+      crypto: "crypto-browserify",
+      util: "rollup-plugin-node-polyfills/polyfills/util",
+      process: "rollup-plugin-node-polyfills/polyfills/process-es6",
+    },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [inject({ Buffer: ["Buffer", "Buffer"] }), rollupNodePolyFill()],
     },
   },
 });
